c772d5de1be01979425eae211687423d418ef786
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-14 19:59:59 +0100

    Desktop Landing Animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingAnimationComponent } from './landing-animation/landing-animation.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingAnimationComponent,
+    LoginpageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/landing-animation/landing-animation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-animation',
+  templateUrl: './landing-animation.component.html',
+  styleUrls: ['./landing-animation.component.sass']
+})
+export class LandingAnimationComponent {
+
+}
