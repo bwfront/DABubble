486f5fbb9ac77cf38235a0851cb33282d4050469
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-15 20:53:56 +0100

    Implement FireBase and Login in Form Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { LandingAnimationComponent } from './landing-animation/landing-animation.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { LogincardComponent } from './logincard/logincard.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { LogincardComponent } from './logincard/logincard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"dabubble-1a43e","appId":"1:913169184248:web:8beb137d5661d06397120d","storageBucket":"dabubble-1a43e.appspot.com","apiKey":"AIzaSyCemzBdnjhG6MBmsJGQVi4-i9RuWLyGGUs","authDomain":"dabubble-1a43e.firebaseapp.com","messagingSenderId":"913169184248"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/logincard/logincard.component.ts
+++ src/app/logincard/logincard.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-logincard',
   templateUrl: './logincard.component.html',
-  styleUrls: ['./logincard.component.sass']
+  styleUrls: ['./logincard.component.sass'],
 })
 export class LogincardComponent {
-  passworderror: boolean = false
-  emailerror: boolean = false
+  passworderror: boolean = false;
+  emailerror: boolean = false;
+
+  userEmail: string = '';
+  userPassword: string = '';
+
+  constructor() {}
+
+  userLogin() {
+    if (this.userEmail && this.userPassword) {
+
+      console.log('Email:', this.userEmail, 'Password:', this.userPassword);
+    }
+  }
 }
